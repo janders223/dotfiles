49aa94b041d5e0b4a9085241c91b063c2baa049e
Jim Anders <jimanders223@gmail.com>
2021-01-15 11:32:26 -0500

    Add zsh command edit mode

--- machines/home.nix
+++ machines/home.nix
@@ -33,7 +33,10 @@ in
       l = "${pkgs.coreutils}/bin/ls -halF";
       vim = "nvim";
     };
-    initExtra = ''
+    initExtraFirst = ''
+    autoload -U edit-command-line
+    zle -N edit-command-line
+    bindkey -M vicmd v edit-command-line
       '';
   };
 
